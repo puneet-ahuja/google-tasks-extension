11cff8ed8d86179e13cbbb3934c007437f1957c1
Puneet Ahuja <puneet.ahuja@olx.com>
2020-03-22 16:28:03 +0530

    Request API data from Google Apis.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import {loadTasksApi, handleAuthClick, handleSignoutClick} from './GoogleAPI'
+
+
+
 
 function App() {
+
+
+  // TODO : Need To implement a header next
+  const [isSignedIn] = useState(false);
+
+  // This Component Will work Similar to Component Did Mount
+  useEffect(loadTasksApi);
+
+
   return (
     <div className="App">
       <div className='column column-1'>Task Lists</div>
       <div className='column column-2'>Task List</div>
       <div className='column column-3'>Task</div>
+      {isSignedIn? <div onClick={handleSignoutClick}>Sign Out</div>: <div onClick={handleAuthClick}>Sign in</div>}
     </div>
   );
 }
