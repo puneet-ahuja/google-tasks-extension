cc025d8045ee5e7d83bc0baf3b78d62745ee5e06
Puneet Ahuja <puneet.ahuja@olx.com>
2020-03-28 19:47:27 +0530

    Added Task List Data to Redux Store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import userDetails from './userDetails';
 import tasklists from './tasklists'
+import tasklist from './tasklist'
 
 export default combineReducers({
     userDetails,
-    tasklists
+    tasklists,
+    tasklist
 });
