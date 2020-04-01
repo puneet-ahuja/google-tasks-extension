352968ebf57230709085f26016f36d6f93c3861b
Puneet Ahuja <puneet.ahuja@olx.com>
2020-04-02 03:49:58 +0530

    Added click listeners on AddListForm

--- src/components/AddListForm/index.js
+++ src/components/AddListForm/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 import PropTypes from 'prop-types'
 import PrimaryButton from '../CommonComponents/PrimaryButton'
 import CancelButton from '../CommonComponents/CancelButton';
 
-const AddListForm = ({isSighnedIn,dispatch}) => {
+const AddListForm = ({ onSaveClick, onCancelClick }) => {
+
+    const [ listName, setListName ] = useState('')
+
+    const enableSave = listName.length > 0;
+
+    const inputChangeHandler = ( event ) => {
+        setListName(event.target.value);
+    }
+
+    const saveClickHandler = () => {
+        onSaveClick && onSaveClick(listName);
+        setListName('');
+        onCancelClick && onCancelClick();
+    }
   
     return (
       <div className='add-list-form-container'>
-          <input type='text' className='add-list-input'/>
+          <input type='text' className='add-list-input' value={listName} placeholder={'Write List Name'} onChange={inputChangeHandler}/>
           <div className='add-list-buttons-container'>
-            <PrimaryButton text={'Save'} onClick={()=>console.log('Save Button Clicked')}></PrimaryButton>
-            <CancelButton text={'Cancel'} onClick={()=>console.log('Save Button Clicked')}></CancelButton>
+            <PrimaryButton text={'Save'} onClick={saveClickHandler} enabled={enableSave}></PrimaryButton>
+            <CancelButton text={'Cancel'} onClick={onCancelClick}></CancelButton>
           </div>
     </div>
     );
